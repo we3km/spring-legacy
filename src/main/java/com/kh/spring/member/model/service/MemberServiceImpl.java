f38fad3099d8a70aef51887fe4b0b162131c71cd
we3km <whdbstlr1662@naver.com>
2025-07-04 18:34:33 +0900

    spring 수업내용 커밋 2020-07-04

--- src/main/java/com/kh/spring/member/model/service/MemberServiceImpl.java
+++ src/main/java/com/kh/spring/member/model/service/MemberServiceImpl.java
@@ -1,12 +1,25 @@
 package com.kh.spring.member.model.service;
 
+import java.util.HashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.kh.spring.member.model.dao.MemberDao;
 import com.kh.spring.member.model.vo.Member;
 
+@Service //Componet scan에 의해 bean 객체로 등록될 클래스를 지정
 public class MemberServiceImpl implements MemberService {
 
+	@Autowired
 	private MemberDao memberDao;
 
+	@Override
+	public Member loginMember(Member m) {
+		
+		return memberDao.loginMember(m);
+	}
+	
 	@Override
 	public Member loginMember(String userId) {
 
@@ -15,25 +28,30 @@ public class MemberServiceImpl implements MemberService {
 
 	@Override
 	public int insertMember(Member m) {
-
-		return 0;
+		return memberDao.insertMember(m);
 	}
 
 	@Override
 	public int updateMember(Member m) {
 
-		return 0;
+		return memberDao.updateMember(m);
 	}
 
 	@Override
 	public int idCheck(String userId) {
-
-		return 0;
+		
+		return memberDao.idCheck(userId);
 	}
 
 	@Override
 	public void updateMemberChagePwd() {
 
 	}
+
+	@Override
+	public HashMap<String, Object> selectOne(String userId) {
+		return memberDao.selectOne(userId);
+	}
+
 
 }
--- src/main/java/com/kh/spring/member/model/vo/Member.java
+++ src/main/java/com/kh/spring/member/model/vo/Member.java
@@ -1,4 +1,36 @@
 package com.kh.spring.member.model.vo;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+
+
+@NoArgsConstructor
+//@AllArgsConstructor
+@Data
+//@Builder
 public class Member {
-	
+	private int userNo;
+	private String userId;
+	private String userPwd;
+	private String userName;
+	private String profileImg;
+	private String email;
+    private String birthday;
+    private String gender;
+    private String phone;
+    private String address;
+    private Date enrollDate;
+    private Date modifyDate;
+    private String status;
+    
+    
+    
+    
+    
 }
